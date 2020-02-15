d336a0b50b535a59c1648086fbac19fa051331cd
Ryan Yosua <ryan.yosua@nutrien.com>
2020-02-15 13:28:01 -0700

    change port

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen(4000).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
